2affa9d925dd97d74dace85dbb58fb741a6cb543
AlexRussovits <sasikrus2002@gmail.com>
2021-03-02 22:05:03 +0200

    rename title

--- resources/views/layout/layout_main.blade.php
+++ resources/views/layout/layout_main.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Mechanics HTML Template | Homepage</title>
+    <title>Punapaju ÖÜ</title>
     <!-- Stylesheets -->
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
